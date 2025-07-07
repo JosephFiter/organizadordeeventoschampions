7d56a44bfad22356cf9675a9154a6c017141f569
Lucasszejn <roguenine6@gmail.com>
2025-07-06 22:25:40 -0300

    ahi

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import './Scheduler.css';
